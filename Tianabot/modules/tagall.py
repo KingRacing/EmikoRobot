c04c4eca8a69fa97080f570da7b04733d65b3eb9
Emilia <88168463+EmiliaTzy@users.noreply.github.com>
2022-08-06 23:46:19 +0700

    Update tagall.py

--- Tianabot/modules/tagall.py
+++ Tianabot/modules/tagall.py
@@ -16,7 +16,7 @@ spam_chats = []
 async def mentionall(event):
   chat_id = event.chat_id
   if event.is_private:
-    return await event.respond("__This command can be use in groups and channels!__")
+    return await event.respond("__Perintah ini dapat digunakan di grup dan channel!__")
   
   is_admin = False
   try:
@@ -38,7 +38,7 @@ async def mentionall(event):
     ):
       is_admin = True
   if not is_admin:
-    return await event.respond("__Only admins can mention all!__")
+    return await event.respond("__Hanya admin yang boleh menandai semua!__")
   
   if event.pattern_match.group(1) and event.is_reply:
     return await event.respond("__Give me one argument!__")
@@ -49,9 +49,9 @@ async def mentionall(event):
     mode = "text_on_reply"
     msg = await event.get_reply_message()
     if msg == None:
-        return await event.respond("__I can't mention members for older messages! (messages which are sent before I'm added to group)__")
+        return await event.respond("__Saya tidak bisa menyebut anggota untuk pesan lama! (pesan yang dikirim sebelum saya ditambahkan ke grup)__")
   else:
-    return await event.respond("__Reply to a message or give me some text to mention others!__")
+    return await event.respond("__Membalas pesan atau memberi saya beberapa teks untuk menyebutkan orang lain!__")
   
   spam_chats.append(chat_id)
   usrnum = 0
@@ -78,7 +78,7 @@ async def mentionall(event):
 @client.on(events.NewMessage(pattern="^/cancel$"))
 async def cancel_spam(event):
   if not event.chat_id in spam_chats:
-    return await event.respond("__There is no proccess on going...__")
+    return await event.respond("__Tidak ada proses berjalan...__")
   is_admin = False
   try:
     partici_ = await client(GetParticipantRequest(
@@ -99,7 +99,7 @@ async def cancel_spam(event):
     ):
       is_admin = True
   if not is_admin:
-    return await event.respond("__Only admins can execute this command!__")
+    return await event.respond("__Hanya admin yang dapat menggunakan perintah ini!__")
   
   else:
     try:
