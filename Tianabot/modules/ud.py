8343c1707f1bede0e68a9f9b3e5005da5d1de25f
Emilia <88168463+EmiliaTzy@users.noreply.github.com>
2022-08-06 23:52:32 +0700

    Update ud.py

--- Tianabot/modules/ud.py
+++ Tianabot/modules/ud.py
@@ -13,9 +13,9 @@ def ud(update: Update, context: CallbackContext):
         f"https://api.urbandictionary.com/v0/define?term={text}"
     ).json()
     try:
-        reply_text = f'*{text}*\n\n{results["list"][0]["definition"]}\n\n_{results["list"][0]["example"]}_'
+        reply_text = f'*{text}*\n\n{results["list"][0]["definisi"]}\n\n_{results["list"][0]["contoh"]}_'
     except:
-        reply_text = "No results found."
+        reply_text = "Hasil tidak ditemukan."
     message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
 
 
