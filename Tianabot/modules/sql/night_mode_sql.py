52f27e82ebb040af260c90647a2d43dc06a11cb3
Emilia <88168463+EmiliaTzy@users.noreply.github.com>
2022-08-10 20:53:12 +0700

    Update night_mode_sql.py

--- Tianabot/modules/sql/night_mode_sql.py
+++ Tianabot/modules/sql/night_mode_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
-from EmikoRobot.modules.sql import BASE, SESSION
+from Tianabot.modules.sql import BASE, SESSION
 
 
 class Nightmode(BASE):
