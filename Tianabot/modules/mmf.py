f050bd395674ae1a4a80a172134a0731543670b0
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-08-23 09:10:49 +0700

    Update mmf.py

--- Tianabot/modules/mmf.py
+++ Tianabot/modules/mmf.py
@@ -10,17 +10,17 @@ async def handler(event):
     if event.fwd_from:
         return
     if not event.reply_to_msg_id:
-        await event.reply("`Provide Some Text To Draw! And Reply To Image/Stickers EXAMPLE: /mmf text`")
+        await event.reply("`Berikan Beberapa Teks Untuk Digambar! Dan Balas Ke Gambar/Stiker CONTOH: /mmf teks`")
         return
     reply_message = await event.get_reply_message()
     if not reply_message.media:
-        await event.reply("```Reply to a image/sticker.```")
+        await event.reply("```Balas ke gambar/stiker.```")
         return
     file = await bot.download_media(reply_message)
-    msg = await event.reply("```Memifying this image! (」ﾟﾛﾟ)｣ ```")
+    msg = await event.reply("```Memifying gambar ini! (」ﾟﾛﾟ)｣ ```")
     text = str(event.pattern_match.group(1)).strip()
     if len(text) < 1:
-        return await msg.edit("You might want to try `/mmf text`")
+        return await msg.edit("Anda mungkin ingin mencoba `/mmf teks`")
     meme = await drawText(file, text)
     await bot.send_file(event.chat_id, file=meme, force_document=False)
     await msg.delete()
