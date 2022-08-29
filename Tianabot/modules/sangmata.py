ab379db3cb4eadec1d5ddf12fa2fd1a8fbf728ff
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-08-30 01:34:41 +0200

    Update sangmata.py

--- Tianabot/modules/sangmata.py
+++ Tianabot/modules/sangmata.py
@@ -1,27 +1,27 @@
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from EmikoRobot import telethn as tbot
-from EmikoRobot.events import register
-from EmikoRobot import ubot2 as ubot
+from Tianabot import telethn as tbot
+from Tianabot.events import register
+from Tianabot import ubot2 as ubot
 from asyncio.exceptions import TimeoutError
 
 
 @register(pattern="^/sg ?(.*)")
 async def lastname(steal):
     steal.pattern_match.group(1)
-    puki = await steal.reply("```Retrieving Such User Information..```")
+    puki = await steal.reply("```Mengambil Informasi Pengguna Tersebut..```")
     if steal.fwd_from:
         return
     if not steal.reply_to_msg_id:
-        await puki.edit("```Please Reply To User Message.```")
+        await puki.edit("```Mohon Balas Ke Pesan Pengguna.```")
         return
     message = await steal.get_reply_message()
     chat = "@SangMataInfo_bot"
     user_id = message.sender.id
     id = f"/search_id {user_id}"
     if message.sender.bot:
-        await puki.edit("```Reply To Real User's Message.```")
+        await puki.edit("```Balas Ke Pesan Pengguna Asli.```")
         return
-    await puki.edit("```Please wait...```")
+    await puki.edit("```Mohon Tunggu...```")
     try:
         async with ubot.conversation(chat) as conv:
             try:
@@ -30,7 +30,7 @@ async def lastname(steal):
                 response = await conv.get_response()
             except YouBlockedUserError:
                 await steal.reply(
-                    "```Error, report to @kenbotsupport```"
+                    "```Error, hubungi @eiko_support```"
                 )
                 return
             if r.text.startswith("Name"):
@@ -40,10 +40,10 @@ async def lastname(steal):
                     conv.chat_id, [msg.id, r.id, response.id, respond.id]
                 ) 
                 return
-            if response.text.startswith("No records") or r.text.startswith(
-                "No records"
+            if response.text.startswith("Tidak Ada Jejak") or r.text.startswith(
+                "Tidak Ada Jejak"
             ):
-                await puki.edit("```I Can't Find This User's Information, This User Has Never Changed His Name Before.```")
+                await puki.edit("```Saya Tidak Dapat Menemukan Informasi Pengguna Ini, Pengguna Ini Belum Pernah Mengubah Namanya Sebelumnya.```")
                 await ubot.delete_messages(
                     conv.chat_id, [msg.id, r.id, response.id]
                 )
@@ -55,4 +55,4 @@ async def lastname(steal):
                 conv.chat_id, [msg.id, r.id, response.id, respond.id]
             )
     except TimeoutError:
-        return await puki.edit("`I'm Sick Sorry...`")
+        return await puki.edit("`Saya Lelah Maaf...`")
