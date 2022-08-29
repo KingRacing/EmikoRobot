70fea2f9ff42ef4019d026e4c16b1b425f0304a0
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-08-30 01:28:15 +0200

    Update alive.py

--- Tianabot/modules/alive.py
+++ Tianabot/modules/alive.py
@@ -9,16 +9,16 @@ from EikoBot.events import register
 from EikoBot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/b2d68d6217bceb7eaa3a7.jpg"
+PHOTO = "https://telegra.ph/file/22fdd1bbe77bc86669b5d.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hai [{event.sender.first_name}](tg://user?id={event.sender.id}), Saya Eiko Bot.** \n\n"
+  TEXT = f"**Hai [{event.sender.first_name}](tg://user?id={event.sender.id}), Saya Eiko Robot.** \n\n"
   TEXT += "⚪ **Saya Bekerja Dengan Benar** \n\n"
   TEXT += f"⚪ **Pengembangku: [Az](https://t.me/tth_kiya98)** \n\n"
   TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
   TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Terima Kasih Telah Menambahkanku Disini ❤️**"
-  BUTTON = [[Button.url("Bantuan", "https://t.me/EikoManager_Bot?start=help"), Button.url("Support", "https://t.me/eikosupport")]]
+  BUTTON = [[Button.url("Bantuan", "https://t.me/EikoManager_Bot?start=help"), Button.url("Support", "https://t.me/eiko_support")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
