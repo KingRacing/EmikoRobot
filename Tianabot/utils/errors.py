6609d8ba54e44fb639c18b1bb708fc5fed59ffbe
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-08-30 02:30:49 +0200

    Update errors.py

--- Tianabot/utils/errors.py
+++ Tianabot/utils/errors.py
@@ -2,7 +2,7 @@
 import sys
 import traceback
 from functools import wraps
-from pyrogramee.errors.exceptions.forbidden_403 import ChatWriteForbidden
+from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 from Tianabot import pbot as app, SUPPORT_CHAT
 
 def split_limits(text):
